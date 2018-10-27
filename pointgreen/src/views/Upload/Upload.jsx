05dd667924849e64e2e3d143ee3f6457a7948f9f
shateeshb <bhugwansing.shateesh@gmail.com>
2018-10-27 02:20:08 -0400

    starting Upload view

---
+++ pointgreen/src/views/Upload/Upload.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'; 
+
+class Upload extends Component {
+    render(){
+        return(
+            <h3>Upload a Photo </h3>
+            
+        )
+    }
+}
+
+export default Upload; 
