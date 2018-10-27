29558a88247fe4a835aa04c016a761013cdab375
khelif96 <khelif96@gmail.com>
2018-10-27 09:26:15 -0400

    Working feed

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ process.on("SIGINT", () => {
 });
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 // Configure app to use bodyParser()
