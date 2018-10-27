b9e75d3b5b4a6cf26eecac31658034e1d8b30a12
wale1958 <waleojetola@gmail.com>
2018-10-26 22:22:35 -0400

    Edited logic

--- backend/controller/puzzleInit.js
+++ backend/controller/puzzleInit.js
@@ -4,5 +4,11 @@ exports.puzzle = (req, res) => {
     const badgeIDs = req.body;
     let newPuzzle = new puzzle("",badgeIDs);
 
+    newPuzzle.save((err, puzzle) => {
+        if(err){
+            res.send(err);
+        }    
+        res.json(puzzle);
+    });
     return newPuzzle;
 }
