8f0bb559ab2373d521547d00e992515b1fe5a9da
shateeshb <bhugwansing.shateesh@gmail.com>
2018-10-27 05:22:08 -0400

    starting business impact page

---
+++ pointgreen/src/views/Business/Business.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Header} from '../../components'
+import './Business.css'
+
+class Business extends Component{
+    
+    render(){
+        return(
+        <div>
+            Hi.
+        </div>)
+    }
+}
+export default Business;
