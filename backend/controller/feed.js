0e624043de4c7440a595483059f59436edcc939b
khelif96 <khelif96@gmail.com>
2018-10-27 11:21:55 -0400

    Updated photo view

--- backend/controller/feed.js
+++ backend/controller/feed.js
@@ -4,7 +4,7 @@ const Badge = require('../models/badges');
 
 exports.getFeed = (req, res) => {
 
-    Photo.find({}, {}, { sort: { 'created_at' : -1 }}, function(err, posts) {
+    Photo.find({}, {}, function(err, posts) {
         var counter =0;
         response =[]
         // posts.forEach((post)=>{
@@ -23,19 +23,22 @@ exports.getFeed = (req, res) => {
         badges.forEach((badge) => {
             badgesArray.push(badge.name);
         })
+        console.log(badgesArray);
         var responseData = [];
+        console.log(posts);
         for(var i = 0; i< posts.length; i++){
             responseData.push({
                 url: posts[i].URL,
                 numVotes: posts[i].voters.length,
                 badgeName: badges[i].name,
-                voteUrl: "http://localhost:3000/api/vote/photoID=" + posts[i]._id
+                voteUrl: "http://localhost:3001/api/photo/vote/photoID=" + posts[i]._id
             })
         }
+        console.log(responseData);
         res.json(responseData)
         // let photoData = {
-        //     URL, 
-        //     badgeName: badge.name, 
+        //     URL,
+        //     badgeName: badge.name,
         //     numVotes:post.voters.length };
         //     console.log(photoData);
         //  response.push(photoData);
@@ -44,5 +47,5 @@ exports.getFeed = (req, res) => {
             res.json(response);
         });
         })
-      
+
 }
