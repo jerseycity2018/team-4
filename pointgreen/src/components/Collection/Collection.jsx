ea827ad4409f2e317c49f8c5f91536b4abfa3dc9
Peggy Li <xpeggyxli@gmail.com>
2018-10-27 08:00:26 -0400

    create collection and badge components

---
+++ pointgreen/src/components/Collection/Collection.jsx
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { Badge } from '..';
+import './Collection.css';
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+});
+
+class Collection extends PureComponent {
+  render(){
+    const { classes, badges, puzzle } = this.props;
+
+    const style = {
+      filter: `grayscale(${100-puzzle.progress}%)`
+    };
+
+    let myButton;
+    if (puzzle.progress === 100)
+      myButton = <Button variant="contained" className="redeemButton">Redeem</Button>;
+    else
+      myButton = <Button variant="contained" disabled>{puzzle.progress}%</Button>;
+
+    return(
+      <div class="Collection">
+        <Paper className={classes.root} elevation={1}>
+          <div class="badgeList">
+            {badges.map((badge) => <Badge type={badge.type} bgColor={badge.bgColor} desc={badge.desc} earned={badge.earned} required={badge.required}/>)}
+          </div>
+          <div className="puzzleInfo">
+            <img style={style} src={puzzle.imgUrl} />
+            <div className="puzzleText">
+              <h3>{puzzle.reward}</h3>
+              <p>{puzzle.company}</p>
+            </div>
+            {myButton}
+          </div>
+        </Paper>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(Collection);
--- pointgreen/src/components/index.js
+++ pointgreen/src/components/index.js
@@ -2,7 +2,8 @@ import NavBar from './NavBar/NavBar';
 import Photo from './Photo/Photo';
 import Brand from './Brand/Brand';
 import Badge from './Badge/Badge';
+import Collection from './Collection/Collection';
 
 export {
-  NavBar, Brand, Photo, Badge,
+  NavBar, Brand, Photo, Badge, Collection,
 };
