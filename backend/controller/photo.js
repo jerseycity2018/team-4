a4a051e4740e1d0a58a898ffa9d11366ae8592ea
wale1958 <waleojetola@gmail.com>
2018-10-27 03:17:46 -0400

    Edited logic

--- backend/controller/photo.js
+++ backend/controller/photo.js
@@ -22,16 +22,14 @@ exports.uploadPhoto = (req, res) => {
 
     exports.votePhoto = (req, res) => {
         console.log(req.params);
-        const {photoID, userID} = req.params;
+        const {photoID} = req.params;
+        const {userID} = req.body;
         Photo.findById(photoID, (err, photo) => {
             if(err) res.status(500).json({error: 'Unable to find photo with that id'}); 
-            else{    
-                console.log(photo);
-                console.log(photoID);
-                console.log(userID);
-                if(photo.voters.includes(userID)) res.status(500).json({error: 'User already voted'}); 
-                photo.voters=photo.voters.push(userID);    
-                console.log(photo.voters);
+            else if(photo.voters.includes(userID)) res.status(500).json({error: 'User already voted'});
+            else{
+                photo.voters.push(userID);    
+                console.log(photo.voters.includes(userID));
                 photo.save(error => {    
                 if (error) res.status(500).json({error: 'internal server error while voting'});
             
