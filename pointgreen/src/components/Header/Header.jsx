f31e3910f49627d1f07465bfca3e96cd8d21830e
Peggy Li <xpeggyxli@gmail.com>
2018-10-26 22:43:08 -0400

    Create header component

---
+++ pointgreen/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Header extends PureComponent {
+    render(){
+        return(
+            <button>Hello World</button>
+        )
+    }
+}
+
+export default Header;
