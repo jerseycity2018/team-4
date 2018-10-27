8f2cdcec38a96f7912e1d4d277807f834cf64a21
wale1958 <waleojetola@gmail.com>
2018-10-26 22:21:32 -0400

    Edited logic

--- backend/controller/badgeInit.js
+++ backend/controller/badgeInit.js
@@ -3,6 +3,11 @@ const badge = require('badge');
 exports.badge = (req, res) => {
     const {name, imgURL} = req.body;
     let newBadge = new badge(name,imgURL);
-
+    newBadge.save((err, badge) => {
+        if(err){
+            res.send(err);
+        }    
+        res.json(badge);
+    });
     return newBadge;
 }
