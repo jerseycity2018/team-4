3de7330fe06a7217f04fe21edc1a59ec16f75898
khelif96 <khelif96@gmail.com>
2018-10-26 20:29:48 -0400

    Set up repo

---
+++ backend/routes/api.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+const router = express.Router();
+
+const apiHome = require('../controller/apiHome');
+const clipBoardManager = require('../controller/clipBoardManager');
+const auth = require('../controller/auth');
+
+// API
+// Base API Route
+router.get('/', apiHome.getApi);
+router.post('/', apiHome.postApi);
+
+
+
+module.exports = router;
